8526716cd9e642d90f82a4f529ccbaa95baabbdb
Norayr <19norayr2001@gmail.com>
2020-11-03 12:21:59 +0400

    added algebra and lisp projects

---
+++ Algebra/Algebra/Main.cpp
@@ -0,0 +1,12 @@
+#include "UnitTests.h"
+#include "Permutation.h"
+#include <iostream>
+
+int main() {
+
+//	Testing::testSimsAlgorithm();
+//	Testing::testAbelianGroupCount();
+//	Testing::testAbelianGroupIsmorphism();
+//	Testing::testEilerFunction();
+	return 0;
+}
