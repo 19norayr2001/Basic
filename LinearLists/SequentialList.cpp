8ab62ab427f346c8b9de7a174030c536880870ee
Norayr <19norayr2001@gmail.com>
2021-06-06 00:45:44 +0400

    refactored SequentialList code, uses swap-copy pattern

--- LinearLists/SequentialList.cpp
+++ LinearLists/SequentialList.cpp
@@ -1,115 +1,94 @@
 #include "SequentialList.h"
 
-static const int MAX_SIZE = 100;
+static const size_t MAX_SIZE = 2;
 
 SequentialList::SequentialList()
-	: m_array(nullptr)
-	, m_maxsize(MAX_SIZE)
+	: m_capacity(MAX_SIZE)
 	, m_size(0)
-{
-	m_array = new int[m_maxsize];	
-}
+	, m_array(new value_type[m_capacity]) {}
 
-SequentialList::SequentialList(const SequentialList& object)
-	: m_maxsize(object.m_maxsize)
-	, m_size(object.m_size)
-	, m_array(nullptr) 
+SequentialList::SequentialList(const SequentialList& obj)
+	: m_capacity(obj.m_capacity)
+	, m_size(obj.m_size)
+	, m_array(new value_type[m_capacity]) 
 {
-	m_array = new int[m_maxsize];
-	for (int i = 0; i < m_size; ++i) {
-		m_array[i] = object.m_array[i];
+	for (size_t i = 0; i < m_size; ++i) {
+		m_array[i] = obj.m_array[i];
 	}
 }
 
-SequentialList& SequentialList::operator=(const SequentialList& object)
-{
-	if (this != &object) {
-		delete[] m_array;
-		m_size = object.m_size;
-		m_maxsize = object.m_maxsize;
-		m_array = new int[m_maxsize];
-		for (int i = 0; i < m_size; ++i) {
-			m_array[i] = object.m_array[i];
-		}
+SequentialList& SequentialList::operator=(const SequentialList& obj) {
+	if (this != &obj) {
+		SequentialList copy_obj(obj);
+		this->swap(copy_obj);
 	}
 	return *this;
 }
 
-SequentialList::~SequentialList()
-{
+SequentialList::~SequentialList() {
 	delete[] m_array;
 }
 
-void SequentialList::push_back(const int& num)
-{
-	if (m_size == m_maxsize) {
-		m_maxsize *= 2;
-		int* p = new int[m_maxsize];
-		for (int i = 0; i < m_size; ++i)
-			p[i] = m_array[i];
-		delete[] m_array;
-		m_array = p;
+void SequentialList::push_back(const value_type& elem) {
+	if (is_full()) {
+		expand_capacity();
 	}
-	m_array[m_size++] = num;
+	m_array[m_size++] = elem;
 }
 
-void SequentialList::pop_back()
-{
+void SequentialList::pop_back() {
 	if (!is_empty()) {
 		--m_size;
 	}
 }
-void SequentialList::insert(int ind, const int& num)
-{
-	if (m_size == m_maxsize)
-	{
-		m_maxsize *= 2;
-		int* p = new int[m_maxsize];
-		for (int i = 0; i < ind; ++i) {
-			p[i] = m_array[i];
-		}
-		p[ind] = num;
-		for (int i = ind + 1; i <= m_size; ++i){
-			p[i] = m_array[i - 1];
-		}
-		m_size++;
-		delete[] m_array;
-		m_array = p;
+void SequentialList::insert(size_t ind, const value_type& elem) {
+	if (ind >= m_size) {
+		push_back(elem);
+		return;
 	}
-	else
-	{
-		int temp = num;
-		m_size++;
-		for (int i = ind; i < m_size; ++i) {
-			std::swap(m_array[i], temp);
-		}
+	value_type last = m_array[m_size - 1];
+	for (size_t i = m_size - 1; i > ind; --i) {
+		m_array[i] = m_array[i - 1];
 	}
+	m_array[ind] = elem;
+	push_back(last);
 }
 
-void SequentialList::remove(int ind)
-{
-	m_size--;
-	for (int i = ind; i < m_size; ++i) {
+void SequentialList::remove(size_t ind) {
+	for (size_t i = ind; i < m_size; ++i) {
 		m_array[i] = m_array[i + 1];
 	}
+	pop_back();
 }
 
-int& SequentialList::operator[](int ind)
-{
+SequentialList::value_type& SequentialList::operator[](size_t ind) {
 	return m_array[ind];
 }
 
-const int& SequentialList::operator[](int ind) const
-{
+const SequentialList::value_type& SequentialList::operator[](size_t ind) const {
 	return m_array[ind];
 }
 
-std::ostream& operator<<(std::ostream& print, SequentialList& obj)
-{
-	for (int i = 0; i < obj.m_size; ++i)
-	{
-		std::cout << obj.m_array[i] << " ";
+void SequentialList::swap(SequentialList& obj) {
+	std::swap(m_capacity, obj.m_capacity);
+	std::swap(m_size, obj.m_size);
+	std::swap(m_array, obj.m_array);
+}
+
+void SequentialList::expand_capacity() {
+	m_capacity *= 2;
+	value_type* ptr = new value_type[m_capacity];
+	for (size_t i = 0; i < m_size; ++i) {
+		ptr[i] = m_array[i];
+	}
+	delete[] m_array;
+	m_array = ptr;
+}
+
+std::ostream& operator<<(std::ostream& out, SequentialList& obj) {
+	for (size_t i = 0; i < obj.m_size; ++i) {
+		std::cout << obj.m_array[i] << ' ';
 	}
 	std::cout << std::endl;
-	return print;
+	return out;
 }
--- LinearLists/SequentialList.h
+++ LinearLists/SequentialList.h
@@ -1,24 +1,32 @@
 #pragma once
 #include <iostream>
+
 class SequentialList
 {
+	typedef int value_type;
 	friend std::ostream& operator<<(std::ostream&, SequentialList&);
 public:
 	SequentialList();	
-	SequentialList(const SequentialList& object);
-	SequentialList& operator=(const SequentialList& object);
+	SequentialList(const SequentialList&);
+	SequentialList& operator=(const SequentialList&);
 	~SequentialList();
 public:
-	int get_size() const { return m_size; }
-	bool is_empty() const { return m_size == 0; }
-	void push_back(const int& num);
+	void push_back(const value_type&);
 	void pop_back();
-	void insert(int ind, const int& num);
-	void remove(int ind);
-	int&  operator[](int ind);
-	const int& operator[](int ind) const;
+	void insert(size_t ind, const value_type&);
+	void remove(size_t ind);
+public:
+	value_type& operator[](size_t);
+	const value_type& operator[](size_t) const;
+public:
+	size_t get_size() const { return m_size; }
+	bool is_empty() const { return m_size == 0; }
 private:
-	int* m_array;
-	int m_maxsize;
-	int m_size;
+	void swap(SequentialList&);
+	void expand_capacity();
+	bool is_full() const { return m_size == m_capacity; }
+private:
+	size_t m_capacity;
+	size_t m_size;
+	value_type* m_array;
 };
