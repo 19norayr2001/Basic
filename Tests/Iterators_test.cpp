80fc5ca537771c2f68fed4a0246ef5b8a6d7b7a7
norayr <19norayr2001@gmail.com>
2022-04-18 00:46:22 +0400

    change iterator tests to gtest tests, remove all old unit tests

---
+++ Tests/Iterators_test.cpp
@@ -0,0 +1,39 @@
+#include "BasicsConfig.h"
+
+#ifdef TEST_ITERATORS
+
+#include <gtest/gtest.h>
+#include <vector>
+#include <algorithm>
+
+#include <back_insert_iterator.h>
+#include <insert_iterator.h>
+
+
+TEST(IteratorTest, BackInserter) {
+    std::vector<int> vec{1, 2, 3, 4, 5, 6};
+    std::vector<int> vecTest{4};
+    std::vector<int> vecStl = vecTest;
+
+    // using our back_inserter
+    std::copy(vec.begin(), vec.end(), Iterator::back_inserter(vecTest));
+    // use STL back_inserter
+    std::copy(vec.begin(), vec.end(), std::back_inserter(vecStl));
+
+    EXPECT_EQ(vecTest, vecStl);
+}
+
+TEST(IteratorTest, Inserter) {
+    std::vector<int> vec{6, 5, 4, 3, 2, 1};
+    std::vector<int> vecTest{ -1, -2, -3, -4, -5, -6 };
+    std::vector<int> vecStl = vecTest;
+
+    // using our inserter
+    std::copy(vec.begin(), vec.end(), Iterator::inserter(vecTest, vecTest.begin() + 3));
+    // use STL inserter
+    std::copy(vec.begin(), vec.end(), std::inserter(vecStl, vecStl.begin() + 3));
+
+    EXPECT_EQ(vecTest, vecStl);
+}
+
+#endif // TEST_ITERATORS
