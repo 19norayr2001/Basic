e960ee32ff5534a75dfc705f1dc5e3a323e3780f
Norayr <19norayr2001@gmail.com>
2022-10-17 03:28:31 +0400

    use new style and add treap test

--- tests/trees_test.cpp
+++ tests/trees_test.cpp
@@ -19,8 +19,18 @@ TEST(TreesTest, Treap) {
     for(auto it = treap.rbegin(); it != treap.rend(); ++it, ++itVec) {
         EXPECT_EQ(*it, *itVec);
     }
-    EXPECT_EQ(treap.orderOfKey(2), 3);
-    EXPECT_EQ(treap.orderOfKey(18), -1);
+    EXPECT_EQ(treap.order_of_key(2), 3);
+    EXPECT_EQ(treap.order_of_key(18), -1);
+}
+
+TEST(TreesTest, PairTreap) {
+    ordered_set<std::pair<int, int>> st;
+    for(int i = 0; i < 1000; ++i) {
+        st.insert({i, i});
+    }
+    st.erase({500, 500});
+    EXPECT_EQ(st.size(), 999);
+    EXPECT_TRUE(!st.contains({500, 500}));
 }
 
 #endif // TEST_TREES
