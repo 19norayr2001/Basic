96e1f64022be12c818b2884f880f70e212b107ce
Norayr <19norayr2001@gmail.com>
2022-10-16 17:27:39 +0400

    reimplemenet Treap test with comparator

--- tests/trees_test.cpp
+++ tests/trees_test.cpp
@@ -8,18 +8,18 @@
 #include <algorithm>
 
 TEST(Trees, Treap) {
-    Treap<int> treap;
+    Treap<int, std::greater<>> treap;
     std::vector<int> vec {1, 2, -5, 1, 10, 2, 1, 1, 6, 11, -1100};
     for(int elem: vec) {
         treap.insert(elem);
     }
     std::sort(vec.begin(), vec.end());
     vec.resize(std::unique(vec.begin(), vec.end()) - vec.begin());
-    std::reverse(vec.begin(), vec.end());
     auto itVec = vec.begin();
     for(auto it = treap.rbegin(); it != treap.rend(); ++it, ++itVec) {
         EXPECT_EQ(*it, *itVec);
     }
+    EXPECT_EQ(treap.orderOfKey(2), 3);
 }
 
 #endif // TEST_TREES
