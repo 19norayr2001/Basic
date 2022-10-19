b66dad006edfba92872e7bf2ac533a8c7bced44d
Norayr <19norayr2001@gmail.com>
2022-10-19 12:36:37 +0400

    fix test and add new test for ordered_map

--- tests/trees_test.cpp
+++ tests/trees_test.cpp
@@ -7,11 +7,11 @@
 #include <ordered_set.h>
 #include <vector>
 #include <algorithm>
+#include <implicit_treap.h>
 
-TEST(TreesTest, Treap) {
-    auto il = {1, 2, -5, 1, 10, 2, 1, 1, 6, 11, -1100};
-    ordered_set<int, std::greater<>> treap(il);
-    std::vector<int> vec(il);
+TEST(TreesTest, OrderedSet) {
+    ordered_set<int, std::greater<>> treap {1, 2, -5, 1, 10, 2, 1, 1, 6, 11, -1100};
+    std::vector<int> vec {1, 2, -5, 1, 10, 2, 1, 1, 6, 11, -1100};
     std::sort(vec.begin(), vec.end());
     vec.resize(std::unique(vec.begin(), vec.end()) - vec.begin());
     EXPECT_EQ(vec.size(), treap.size());
@@ -23,7 +23,7 @@ TEST(TreesTest, Treap) {
     EXPECT_EQ(treap.order_of_key(18), treap.size());
 }
 
-TEST(TreesTest, PairTreap) {
+TEST(TreesTest, OrderesSetPair) {
     ordered_set<std::pair<int, int>> st;
     for(int i = 0; i < 1000; ++i) {
         st.emplace(i, i);
@@ -31,6 +31,17 @@ TEST(TreesTest, PairTreap) {
     st.erase({500, 500});
     EXPECT_EQ(st.size(), 999);
     EXPECT_TRUE(!st.contains({500, 500}));
+}
+
+TEST(TreesTest, OrderedMap) {
+    ordered_map<int, int> mp;
+    for(int i = 0; i < 1000; ++i) {
+        mp[i] = i + 1;
+    }
+    for(int i = 0; i < 1000; ++i) {
+        EXPECT_EQ(mp.order_of_key(i), i);
+        EXPECT_EQ(mp.key_of_order(i), i);
+    }
 }
 
 #endif // TEST_TREES
