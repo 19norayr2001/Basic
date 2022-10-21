1ce8b887c273bbcb62f68181afc5b06536c32ebc
Norayr <19norayr2001@gmail.com>
2022-10-22 01:25:35 +0400

    use _ instead of m_ for private data members

--- linear_lists/vector.h
+++ linear_lists/vector.h
@@ -15,10 +15,10 @@ public:
     typedef T value_type;
     typedef std::allocator_traits<Alloc> AllocTraits;
 private:
-    size_t m_capacity;
-    size_t m_size;
-    Alloc m_allocator;
-    value_type* m_array;
+    size_t _capacity;
+    size_t _size;
+    Alloc _allocator;
+    value_type* _array;
 private:
     template<bool B>
     class common_iterator {
@@ -27,7 +27,7 @@ private:
     public:
         using value_type = std::conditional_t<B, const vector::value_type, vector::value_type>;
     private:
-        value_type* m_ptr;
+        value_type* _ptr;
     public:
         explicit common_iterator(value_type*);
 
@@ -104,9 +104,9 @@ public:
 public:
     void reserve(size_t);
 
-    value_type& back() { return m_array[m_size - 1]; }
+    value_type& back() { return _array[_size - 1]; }
 
-    const value_type& back() const { return m_array[m_size - 1]; }
+    const value_type& back() const { return _array[_size - 1]; }
 
 public:
     iterator begin();
@@ -140,14 +140,14 @@ public:
     const value_type& operator[](size_t) const;
 
 public:
-    size_t size() const { return m_size; }
+    size_t size() const { return _size; }
 
-    bool empty() const { return m_size == 0; }
+    bool empty() const { return _size == 0; }
 
     void swap(vector&);
 
 private:
-    bool is_full() const { return m_size == m_capacity; }
+    bool is_full() const { return _size == _capacity; }
 
     void destroy();
 
@@ -163,17 +163,17 @@ std::ostream& operator<<(std::ostream&, const vector<T, Alloc>&);
 template<typename T, typename Alloc>
 template<bool B>
 vector<T, Alloc>::common_iterator<B>::common_iterator(value_type* ptr)
-        :m_ptr(ptr) {}
+        :_ptr(ptr) {}
 
 template<typename T, typename Alloc>
 template<bool B>
 vector<T, Alloc>::common_iterator<B>::common_iterator(const common_iterator<false>& other)
-        :m_ptr(other.m_ptr) {}
+        :_ptr(other._ptr) {}
 
 template<typename T, typename Alloc>
 template<bool B>
 typename vector<T, Alloc>::template common_iterator<B>& vector<T, Alloc>::common_iterator<B>::operator++() {
-    ++m_ptr;
+    ++_ptr;
     return *this;
 }
 
@@ -181,21 +181,21 @@ template<typename T, typename Alloc>
 template<bool B>
 typename vector<T, Alloc>::template common_iterator<B> vector<T, Alloc>::common_iterator<B>::operator++(int)& {
     common_iterator iter = *this;
-    ++m_ptr;
+    ++_ptr;
     return iter;
 }
 
 template<typename T, typename Alloc>
 template<bool B>
 typename vector<T, Alloc>::template common_iterator<B>& vector<T, Alloc>::common_iterator<B>::operator+=(ptrdiff_t n) {
-    m_ptr += n;
+    _ptr += n;
     return *this;
 }
 
 template<typename T, typename Alloc>
 template<bool B>
 typename vector<T, Alloc>::template common_iterator<B>& vector<T, Alloc>::common_iterator<B>::operator--() {
-    --m_ptr;
+    --_ptr;
     return *this;
 }
 
@@ -203,69 +203,69 @@ template<typename T, typename Alloc>
 template<bool B>
 typename vector<T, Alloc>::template common_iterator<B> vector<T, Alloc>::common_iterator<B>::operator--(int)& {
     common_iterator iter = *this;
-    --m_ptr;
+    --_ptr;
     return iter;
 }
 
 template<typename T, typename Alloc>
 template<bool B>
 typename vector<T, Alloc>::template common_iterator<B>& vector<T, Alloc>::common_iterator<B>::operator-=(ptrdiff_t n) {
-    m_ptr -= n;
+    _ptr -= n;
     return *this;
 }
 
 template<typename T, typename Alloc>
 template<bool B>
 ptrdiff_t vector<T, Alloc>::common_iterator<B>::operator-(const common_iterator<B>& iter) const {
-    return m_ptr - iter.m_ptr;
+    return _ptr - iter._ptr;
 }
 
 template<typename T, typename Alloc>
 template<bool B>
 auto vector<T, Alloc>::common_iterator<B>::operator*() const -> value_type& {
-    return *m_ptr;
+    return *_ptr;
 }
 
 template<typename T, typename Alloc>
 template<bool B>
 auto vector<T, Alloc>::common_iterator<B>::operator->() const -> value_type* {
-    return m_ptr;
+    return _ptr;
 }
 
 template<typename T, typename Alloc>
 template<bool B>
 bool vector<T, Alloc>::common_iterator<B>::operator==(const common_iterator<B>& iter) const {
-    return m_ptr == iter.m_ptr;
+    return _ptr == iter._ptr;
 }
 
 template<typename T, typename Alloc>
 template<bool B>
 bool vector<T, Alloc>::common_iterator<B>::operator!=(const common_iterator<B>& iter) const {
-    return m_ptr != iter.m_ptr;
+    return _ptr != iter._ptr;
 }
 
 template<typename T, typename Alloc>
 template<bool B>
 bool vector<T, Alloc>::common_iterator<B>::operator<(const common_iterator<B>& iter) const {
-    return m_ptr < iter.m_ptr;
+    return _ptr < iter._ptr;
 }
 
 template<typename T, typename Alloc>
 template<bool B>
 bool vector<T, Alloc>::common_iterator<B>::operator>(const common_iterator<B>& iter) const {
-    return m_ptr > iter.m_ptr;
+    return _ptr > iter._ptr;
 }
 
 template<typename T, typename Alloc>
 template<bool B>
 bool vector<T, Alloc>::common_iterator<B>::operator<=(const common_iterator<B>& iter) const {
-    return m_ptr <= iter.m_ptr;
+    return _ptr <= iter._ptr;
 }
 
 template<typename T, typename Alloc>
 template<bool B>
 bool vector<T, Alloc>::common_iterator<B>::operator>=(const common_iterator<B>& iter) const {
-    return m_ptr >= iter.m_ptr;
+    return _ptr >= iter._ptr;
 }
 
 template<typename T, typename Alloc>
@@ -288,28 +288,28 @@ vector<T, Alloc>::common_iterator<B>::operator-(ptrdiff_t n) const {
 
 template<typename T, typename Alloc>
 vector<T, Alloc>::vector(const Alloc& allocator)
-        : m_capacity(MAX_SIZE), m_size(0), m_allocator(allocator),
-          m_array(AllocTraits::allocate(m_allocator, m_capacity)) {}
+        : _capacity(MAX_SIZE), _size(0), _allocator(allocator),
+          _array(AllocTraits::allocate(_allocator, _capacity)) {}
 
 template<typename T, typename Alloc>
 vector<T, Alloc>::vector(const vector& obj)
-        : m_capacity(obj.m_capacity), m_size(obj.m_size), m_allocator(obj.m_allocator),
-          m_array(AllocTraits::allocate(m_allocator, m_capacity)) {
+        : _capacity(obj._capacity), _size(obj._size), _allocator(obj._allocator),
+          _array(AllocTraits::allocate(_allocator, _capacity)) {
     // variable is initialized here for catch block
     size_t i = 0;
     try {
-        for (i = 0; i < m_size; ++i) {
+        for (i = 0; i < _size; ++i) {
             // try to construct
-            AllocTraits::construct(m_allocator, m_array + i, obj.m_array[i]);
+            AllocTraits::construct(_allocator, _array + i, obj._array[i]);
         }
     }
     catch (...) {
         // destroy all already constructed objects
         for (; i > 0; --i) {
-            AllocTraits::destroy(m_allocator, m_array + i - 1);
+            AllocTraits::destroy(_allocator, _array + i - 1);
         }
         // deallocate memory
-        AllocTraits::deallocate(m_allocator, m_array, m_capacity);
+        AllocTraits::deallocate(_allocator, _array, _capacity);
         // rethrow caught exception
         throw;
     }
@@ -318,10 +318,10 @@ vector<T, Alloc>::vector(const vector& obj)
 
 template<typename T, typename Alloc>
 vector<T, Alloc>::vector(vector<T, Alloc>&& obj) noexcept
-        :m_capacity(obj.m_capacity), m_size(obj.m_size), m_allocator(obj.m_allocator), m_array(obj.m_array) {
-    obj.m_capacity = 0;
-    obj.m_size = 0;
-    obj.m_array = nullptr;
+        :_capacity(obj._capacity), _size(obj._size), _allocator(obj._allocator), _array(obj._array) {
+    obj._capacity = 0;
+    obj._size = 0;
+    obj._array = nullptr;
 }
 
 template<typename T, typename Alloc>
@@ -352,10 +352,10 @@ template<typename... Args>
 void vector<T, Alloc>::emplace_back(Args&& ... args) {
     // reserve more capacity if current is fully filled
     if (is_full()) {
-        reserve(2 * m_capacity);
+        reserve(2 * _capacity);
     }
-    AllocTraits::construct(m_allocator, m_array + m_size, std::forward<Args>(args)...);
-    ++m_size;
+    AllocTraits::construct(_allocator, _array + _size, std::forward<Args>(args)...);
+    ++_size;
 }
 
 template<typename T, typename Alloc>
@@ -371,23 +371,23 @@ void vector<T, Alloc>::push_back(value_type&& elem) {
 template<typename T, typename Alloc>
 void vector<T, Alloc>::pop_back() {
     if (!empty()) {
-        --m_size;
-        AllocTraits::destroy(m_allocator, m_array + m_size);
+        --_size;
+        AllocTraits::destroy(_allocator, _array + _size);
     }
 }
 
 template<typename T, typename Alloc>
 void vector<T, Alloc>::reserve(size_t n) {
     // if there already present enought memory, just return
-    if (n <= m_capacity) return;
+    if (n <= _capacity) return;
 
     // allocate and construct new memory
-    value_type* new_array = AllocTraits::allocate(m_allocator, n);
+    value_type* new_array = AllocTraits::allocate(_allocator, n);
     // variable initialized here for catch block
     size_t i = 0;
     try {
-        for (i = 0; i < m_size; ++i) {
-            AllocTraits::construct(m_allocator, new_array + i, std::move_if_noexcept(m_array[i]));
+        for (i = 0; i < _size; ++i) {
+            AllocTraits::construct(_allocator, new_array + i, std::move_if_noexcept(_array[i]));
         }
     }
     catch (...) {
@@ -396,33 +396,33 @@ void vector<T, Alloc>::reserve(size_t n) {
             // if move assignment is safe, then move in old memory
             if constexpr (std::is_nothrow_move_constructible_v<value_type>
                           && std::is_nothrow_move_assignable_v<value_type>) {
-                m_array[i] = new_array[i];
+                _array[i] = new_array[i];
                 continue;
             } else {
                 // else destroy the element
-                AllocTraits::destroy(m_allocator, new_array + i - 1);
+                AllocTraits::destroy(_allocator, new_array + i - 1);
             }
         }
         // deallocate memory
-        AllocTraits::deallocate(m_allocator, new_array, n);
+        AllocTraits::deallocate(_allocator, new_array, n);
         // rethrow caught exception
         throw;
     }
 
     destroy();
 
-    m_capacity = n;
-    m_array = new_array;
+    _capacity = n;
+    _array = new_array;
 }
 
 template<typename T, typename Alloc>
 typename vector<T, Alloc>::iterator vector<T, Alloc>::begin() {
-    return iterator(m_array);
+    return iterator(_array);
 }
 
 template<typename T, typename Alloc>
 typename vector<T, Alloc>::iterator vector<T, Alloc>::end() {
-    return iterator(m_array + m_size);
+    return iterator(_array + _size);
 }
 
 template<typename T, typename Alloc>
@@ -437,12 +437,12 @@ typename vector<T, Alloc>::const_iterator vector<T, Alloc>::cend() const {
 
 template<typename T, typename Alloc>
 typename vector<T, Alloc>::const_iterator vector<T, Alloc>::begin() const {
-    return const_iterator(m_array);
+    return const_iterator(_array);
 }
 
 template<typename T, typename Alloc>
 typename vector<T, Alloc>::const_iterator vector<T, Alloc>::end() const {
-    return const_iterator(m_array + m_size);
+    return const_iterator(_array + _size);
 }
 
 template<typename T, typename Alloc>
@@ -477,28 +477,28 @@ typename vector<T, Alloc>::const_reverse_iterator vector<T, Alloc>::rend() const
 
 template<typename T, typename Alloc>
 typename vector<T, Alloc>::value_type& vector<T, Alloc>::operator[](size_t ind) {
-    return m_array[ind];
+    return _array[ind];
 }
 
 template<typename T, typename Alloc>
 const typename vector<T, Alloc>::value_type& vector<T, Alloc>::operator[](size_t ind) const {
-    return m_array[ind];
+    return _array[ind];
 }
 
 template<typename T, typename Alloc>
 void vector<T, Alloc>::swap(vector<T, Alloc>& obj) {
-    std::swap(m_capacity, obj.m_capacity);
-    std::swap(m_size, obj.m_size);
-    std::swap(m_array, obj.m_array);
+    std::swap(_capacity, obj._capacity);
+    std::swap(_size, obj._size);
+    std::swap(_array, obj._array);
 }
 
 template<typename T, typename Alloc>
 void vector<T, Alloc>::destroy() {
     // destroy elements and deallocate memory
-    for (size_t i = m_size; i > 0; --i) {
-        AllocTraits::destroy(m_allocator, m_array + i - 1);
+    for (size_t i = _size; i > 0; --i) {
+        AllocTraits::destroy(_allocator, _array + i - 1);
     }
-    AllocTraits::deallocate(m_allocator, m_array, m_capacity);
+    AllocTraits::deallocate(_allocator, _array, _capacity);
 }
 
 template<typename T, typename Alloc>
